34e77e3027f7b6c73b21ea3c49400b8ea36270fa
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-01-28 04:05:25 +0100

    Add index and made compatible with kie@0.0.2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const config = {
     hostname: process.env.NODE_SERVER_HOST || 'localhost',
-    port: process.env.NODE_SERVER_PORT || 3000
+    port: process.env.npm_package_config_server_port || 65500
 };
 
 const express = require('express');
@@ -12,15 +12,29 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const server = express();
 const routes = require('./server/routes');
+const serveIndex = require('serve-index');
+
 
 const Archiver = require('archiver');
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
-server.use(express.static(path.join(__dirname ,'/dist')));
+server.use(
+  '/',
+  express.static('./dist'),
+  serveIndex(
+    './dist',
+    {
+      'icons': true,
+      'template': path.join(__dirname, 'index-template', 'index.html'),
+      'stylesheet': undefined
+    }
+  )
+);
+
 server.use(routes);
 
 server.listen(config, () => {
-  console.log(`Server running at http://${config.hostname}:${config.port}/`);
+  const url = `http://${config.hostname}:${config.port}/`;
 });
 
